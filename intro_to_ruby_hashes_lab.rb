25350d82739bd1a2a79d01683d0e7484113b0ca7
Sergio Perez <sergio13prez@gmail.com>
2019-08-13 21:01:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,12 +27,11 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :name => "something",
+        :names => "something",
         :key2 => "something",
         :key3 => "something",
         :key4 => "something"
       }
-      :names => "something"
     }
 	}
 end

ef6188621b724c138546c4faf0e37acb4e8e78cc
Sergio Perez <sergio13prez@gmail.com>
2019-08-13 21:07:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
-        :key3 => "something",
+        :three_pieces_owned => 100,
         :key4 => "something"
       },
     :names => {
